61d83a5ec11b249ea42902f5d9334b4c66d739aa
Curt Arbtin <arbtin@gmail.com>
2017-01-29 19:33:03 -0600

    LanguageFeatures
    Added Nullable, auto init, changing examples

---
+++ Examples/LanguageFeatures/src/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LanguageFeatures.Models;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new string[] { "C#", "Language", "Feautures" });
+        }
+
+        public IActionResult AboutNulls()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "No Name";
+                // Since Related ties the Product implementations/objects togeather e.g."Related" 
+                //  we can also set nullable on
+                // simply we are allowing the related "chained" objecct to be nullable
+                decimal? price = p?.Price ?? 0;
+                // Notice the use of the ? in the .net chain
+                string relatedName = p?.Related?.Name ?? "None";
+                // If we dont specify we get the Auto set property from the model
+                string category = p?.Category ?? "None";
+
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}, Category: {3}", name, price, relatedName, category));
+            }
+
+            return View(results);
+       }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ Examples/LanguageFeatures/src/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        // Auto initialize the property using the "=" for assignment
+        public string Category { get; set; } = "Watersports";
+        // The ? defines the this type can be Nullable
+        public decimal? Price { get; set; }
+        public Product Related { get; set; }
+
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product { Name = "kayak", Category = "Water Craft", Price = 275M };
+            Product lifejacket = new Product { Name = "Lifejacket", Price = 48.95M };
+
+            kayak.Related = lifejacket;
+
+            // Notice here at the end we are adding a null - Empty Product class item to the Product Array
+            return new Product[] { kayak, lifejacket, null };
+        }
+    }
+}
